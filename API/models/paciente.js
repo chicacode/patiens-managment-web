0248acd4c28c6834709fd45f5ef42ea38b21ecca
Geraldine <gerahernan.al@gmail.com>
2019-12-18 11:52:39 +0100

    added a models folder with with Schema with fields

---
+++ API/models/paciente.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //  crea la estructura en la base de datos
+
+// Schema es una nueva tabla de Bases de Datos con campos:
+const pacientesSchema = new Schema({
+    nombre :{
+        type: String,
+        trim: true
+    }, 
+    propietario: {
+        type: String, 
+        trim: true
+    }, 
+    fecha: {
+        type: String,
+        trim: true // para que corte espacios en blanco
+    }, 
+    hora: {
+        type: String, 
+        trim: true
+    },
+    sintomas: {
+        type: String,
+        trim: true
+    }
+});
+// con esta funcion se crea el modelo
+module.exports = mongoose.model('Paciente', pacientesSchema); // exportar el modulo para usarlo en otros archisvos
