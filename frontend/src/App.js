7344b9a12f8a6e6f99bfe52c202c870fecbdf9c8
Geraldine <gerahernan.al@gmail.com>
2019-12-20 20:47:35 +0100

    added hooks useEffect and use State

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import cliente de Axios
+import clienteAxios from './config/axios';
 
 // Componentes
 import Pacientes from './components/Pacientes';
@@ -7,7 +10,19 @@ import NuevaCita from './components/NuevaCita';
 import Cita from './components/Cita';
 
 function App() {
-  // codigo JS - variables de entorno
+  // State de la App - ARRAY destructuring
+  const [citas, guardarCitas] = useState([]); // valor inicial de arreglo vacio
+
+  // Si cambia alguna dependencia se ejecuta el useEffect
+  // Esta funcion se ejecuta automaticamente cuando se carga la App
+  // o cuando realiza algun cambio
+  useEffect(() => { // arrow function esto sustituye a los ciclos de vida
+    
+    return () => {
+      
+    };
+  }, [])
+
   return (
     <Router>
       <Switch>
--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -4,6 +4,7 @@ const clienteAxios =axios.create({
     // La proxima vez que importemos este componente
     // clienteAxios ya tendra acceso a la url de 
     //localhost:5000 del backend  API
+    // Se podran utilizar las rutas sin tener que escribir el dominio
     baseURL: process.env.REACT_APP_BACKEND_URL
 });
 
