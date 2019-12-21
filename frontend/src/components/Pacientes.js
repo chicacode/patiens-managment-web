fc8b89720dbb0462fb88e5a6fbaf9ac02101a26f
Geraldine <gerahernan.al@gmail.com>
2019-12-21 07:38:34 +0100

    added fragments and div structure in Pacientes c

--- frontend/src/components/Pacientes.js
+++ frontend/src/components/Pacientes.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 //sfc
 const Pacientes = (props) => {
-    console.log("Hoola", props);
-    return ( <h1>Desde Pacientes</h1> );
-}
+    if(props.citas.length === 0) return null;
+
+    return ( 
+        <Fragment>
+            <h1 className="my-5">Administrador de Pacientes</h1>
+
+            <div  className="container mt-5 py-5">
+                <div className="row">
+                    <div className="col-12 mb-5 d-flex justify-content-center">
+                        <a href="#" className="btn btn-success text-uppercase py-2 px-5 font-weight-bold">Crear Cita
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </Fragment> );
+} 
 
 export default Pacientes;
