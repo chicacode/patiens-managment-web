29adf7cdd33080e5090e7773c30ca18d867a3972
Geraldine <gerahernan.al@gmail.com>
2020-01-14 19:30:44 +0100

    added style width height to the window

--- Desktop/index.js
+++ Desktop/index.js
@@ -4,7 +4,13 @@ let appWindow;
 
 function crearVentana() {
     appWindow = new BrowserWindow({
-
+        width: 1200,
+        height: 800, 
+        minWidth: 800,
+        minHeight: 600,
+        center: true, 
+        show: false
+        // resizable: false
     });
     // Cuando la app es cerrada.
     appWindow.on('closed', () => {
