efab5eba9533686ad1f0054fc0c7bde70d48c483
Geraldine <gerahernan.al@gmail.com>
2021-07-15 14:31:18 +0200

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'; // Hooks
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import app from './App.css';
 
 // import cliente de Axios
 import clienteAxios from './config/axios';
@@ -15,64 +16,65 @@ function App() {
   const [citas, guardarCitas] = useState([]); // valor inicial de arreglo vacio
   const [consultar, guardarConsulta] = useState(true);
 
-  // Si cambia alguna dependencia se ejecuta el useEffect
-  // Esta funcion se ejecuta automaticamente cuando se carga la App
-  // o cuando realiza algun cambio
-  useEffect(() => { // arrow function esto sustituye a los ciclos de vida
-    // Buen lugar para consumir APIS
-    if(consultar){
-      const consultarAPI = () => {
-        // esta variable clienteAxios es para no escribir la URL completa
-        clienteAxios.get('/pacientes')
-          .then(response => {
-            // colocar en el State el resultado
-            guardarCitas(response.data);
 
-            // Deshabilitar la consulta
-            guardarConsulta(false); // deja de ejecutarse el if
-          })
-          .catch(error => {
-            console.log(error);
-          })
-      }
-      consultarAPI(); // Se llama funcion
-    }
-  }, [consultar] ); // Es para que React este atento a los cambios
-  // cada vez que cambie que se ejecute de nuevo
+
+  // useEffect(() => { // arrow function esto sustituye a los ciclos de vida
+  //   
+  //   if(consultar){
+  //     const consultarAPI = () => {
+  //       // esta variable clienteAxios es para no escribir la URL completa
+  //       clienteAxios.get('/pacientes')
+  //         .then(response => {
+  //           // colocar en el State el resultado
+  //           guardarCitas(response.data);
+
+  //           // Deshabilitar la consulta
+  //           guardarConsulta(false); // deja de ejecutarse el if
+  //         })
+  //         .catch(error => {
+  //           console.log(error);
+  //         })
+  //     }
+  //     consultarAPI(); // Se llama funcion
+  //   }
+  // }, [consultar] ); 
 
   return (
-    <Router>
-      <Switch>
-        <Route 
-          exact 
-          path="/" 
-          component={ () => <Pacientes citas={citas} />}
-        />
-        <Route 
-          exact 
-          path="/new" 
-          component={ () =>  <NuevaCita guardarConsulta={guardarConsulta} />} // Para pasar props a componentes
-        />
-        <Route 
-          exact 
-          path="/cita/:id" 
-          // render permite poner codigo con callback
-          render={(props) =>{
-            // HOF Higher Order Function
-            console.log(props.match.params.id);
-            const cita = citas.filter(cita => cita._id === props.match.params.id)
+    <div>
+      <h1>Bienvenido a la gestión de pacientes</h1>
+    </div>
+    // <Router>
+    //   <Switch>
+    //     <Route 
+    //       exact 
+    //       path="/" 
+    //       component={ () => <Pacientes citas={citas} />}
+    //     />
+    //     <Route 
+    //       exact 
+    //       path="/new" 
+    //       component={ () =>  <NuevaCita guardarConsulta={guardarConsulta} />} // Para pasar props a componentes
+    //     />
+    //     <Route 
+    //       exact 
+    //       path="/cita/:id" 
+    //       // render permite poner codigo con callback
+    //       render={(props) =>{
+    //         // HOF Higher Order Function
+    //         console.log(props.match.params.id);
+    //         const cita = citas.filter(cita => cita._id === props.match.params.id)
           
-            return(
-              <Cita 
-              cita={cita[0]} 
-              guardarConsulta={guardarConsulta}
-              />
-            )
-          }}
+    //         return(
+    //           <Cita 
+    //           cita={cita[0]} 
+    //           guardarConsulta={guardarConsulta}
+    //           />
+    //         )
+    //       }}
           
-        />
-      </Switch>
-    </Router>
+    //     />
+    //   </Switch>
+    // </Router>
   );
 }
 
