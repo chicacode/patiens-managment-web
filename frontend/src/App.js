24270ff306e66590dd93054df3e6b5f6d75ccd87
Geraldine <gerahernan.al@gmail.com>
2021-07-16 11:55:06 +0200

    Added button to delete appointment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,6 +38,7 @@ function App() {
                  appointment={appointment}
                />
              ))}
+        
           </div>
 
         </div>
--- frontend/src/components/Appointment.js
+++ frontend/src/components/Appointment.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import img from '../assets/img/img.jpg';
 
 const Appointment = ({ appointment }) => {
     const { petName, ownerName, date, time, symptoms } = appointment;
     return (
-        <div className="card" style={{ width: "50rem;",border: '1px solid black', borderRadius: "10px!important" }}>
-            <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "180px" }} />
-            <div className="card-body">
-                <p className="card-text">
-                    <span>Pet: {petName}</span>
-                </p>
-                <p className="card-text">
-                    <span> Owner: {ownerName}</span>
-                </p>
-                <p className="card-text">
-                    <span>Date: {date}</span>
-                </p>
-                <p className="card-text">
-                    <span>Hour: {time}</span>
-                </p>
-                <p className="card-text">
-                    <span> Symptoms: {symptoms}</span>
-                </p>
-            </div>
-        </div>
+        <Fragment>
+            <div className="card" style={{ width: "50rem", border: '1px solid black', borderRadius: "10px!important" }}>
+                <img className="card-img-top" src={img} alt="Card image cap" style={{ width: "180px" }} />
+                <div className="card-body">
+                    <p className="card-text">
+                        <span>Pet: {petName}</span>
+                    </p>
+                    <p className="card-text">
+                        <span> Owner: {ownerName}</span>
+                    </p>
+                    <p className="card-text">
+                        <span>Date: {date}</span>
+                    </p>
+                    <p className="card-text">
+                        <span>Hour: {time}</span>
+                    </p>
+                    <p className="card-text">
+                        <span> Symptoms: {symptoms}</span>
+                    </p>
+                </div>
+                <button className="btn btn-primary">Delete Appointment</button>
 
+            </div>
+            <br />
+        </Fragment>
     );
 }
 
