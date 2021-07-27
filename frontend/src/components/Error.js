d984c1f60d565973f4f6a4de1d9d70e0a719dfc8
Geraldine <gerahernan.al@gmail.com>
2021-07-27 21:37:58 +0200

    Added Error componente

---
+++ frontend/src/components/Error.js
@@ -0,0 +1,7 @@
+import React  from 'react';
+
+const Error = ({message}) =>(
+    <p className="alert alert-danger error">{message}</p>
+);
+
+export default Error;
--- frontend/src/components/InputForm.js
+++ frontend/src/components/InputForm.js
@@ -1,26 +1,36 @@
 import React, { Fragment, useState } from 'react';
+import Error  from './Error';
 
 const InputForm = () => {
     const [state, setState] = useState(0);
+    const [error, setError] = useState(false);
 
     const handleChange = e => {
         setState( parseInt(e.target.value))
     }
 
     const setBudget = e => {
-        e.preventDefaul();
+
+        e.preventDefault();
         console.log(e)
 
         //validate
+        if(state < 1 || isNaN(state)){
+            setError(true);
+            return;
+        }
 
 
-        //ok validate
+        // //ok validate
+        setError(false);
     }
     return(
         <Fragment>
             <h2>Enter you budget</h2>
+            {error ? <Error message="Error: There's an Error. Enter a valid number" /> : null}
             <form 
-                 onSubmit={setBudget}>
+                 onSubmit={setBudget}
+            >
                 <input 
                     type="number"
                     className="u-full-width"
